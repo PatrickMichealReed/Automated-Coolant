1c669c2b3917d558bd9ad2321b6ffdff7df40a25
PatrickMichealReed <46830220+PatrickMichealReed@users.noreply.github.com>
2021-09-17 17:04:55 -0700

    Add files via upload

---
+++ Universum_TFTColours.h
@@ -0,0 +1,18 @@
+// Universum | Universum Projects > TFTHistoryGraph | Universum_TFTColours
+
+// Andrei FLorian 14/JUN/2018
+
+// NOTE - This header file contains the colour codes for a variety of colours
+
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+#define LIGHT_GREY      0xBDF7
+#define DARK_GREY       0x7BEF
+#define ORANGE          0xFBE0
+#define BROWN           0x79E0
